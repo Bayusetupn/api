81a4d27094de8ec94953d0ccdaf0b3660e2813ab
bayu3541732 <exmotion.id@gmail.com>
2023-07-07 07:54:40 +0700

    db config

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv'
 dotenv.config()
 
-const db = new Sequelize(process.env.DBNAME,process.env.DBUSER,process.env.DBPASSWORD,{
-    host: process.env.DBHOST,
+const db = new Sequelize('u7664108_db_hisar','u7664108_admin','u7664108_db_hisar',{
+    host: "api-hisar.fullsnackdesigner.com",
     dialect: 'mysql',
     timezone: '+07:00',
     pool: {
