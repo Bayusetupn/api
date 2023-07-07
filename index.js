acf2c8971c0a60c0fe767b4f05d0c0d91e8d046d
bayu3541732 <exmotion.id@gmail.com>
2023-07-07 08:06:55 +0700

    bae

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://hisar-lake.vercel.app/",
+    origin: "https://hisar-lake.vercel.app",
     credentials: true
 }))
 
