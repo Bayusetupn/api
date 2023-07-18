36975b1f7ed377534ee6b3b4ce0ea411c4246ebf
Bayusetupn <22081010027@student.upnjatim.ac.id>
2023-07-18 07:11:49 +0700

    cors

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://hisar.my.id",
     credentials: true
 }))
 
